412dcf51897da97c00ce749e75fed332b773740a
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-08-28 09:22:27 +0800

    Commit 1

---
+++ src/Digits.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Digits {
+    private ArrayList<Integer> digitList;
+
+    public Digits(int num) throws IllegalArgumentException{
+        if (num<0){
+            throw new IllegalArgumentException("The number passed to the constructor ("+num+") cannot be negative!");
+        }
+        String number = num + "";
+        this.digitList = new ArrayList<Integer>(number.length());
+        for (int i =0; i < number.length(); i++){
+            this.digitList.add(Integer.valueOf(number.charAt(i))-48);
+        }
+    }
+
+
+    public boolean isStrictlyIncreasing() {
+        for (int i = 0; i < digitList.size()-1; i++){
+           if (digitList.get(i)>= digitList.get(i+1)) {
+               return false;
+           }
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> getDigitList() {
+        return digitList;
+    }
+}
